d63c1de4b338072ccbd1514508096a1fbcbc89b3
Mai Kuraki <mai.k@live.com>
2025-03-14 22:47:13 +0800

    Make CycloneGames' code as Unity package

---
+++ UnityStarter/Assets/ThirdParty/CycloneGames/CycloneGames.GameplayFramework/Samples/Sample.PureUnity/UnitySampleObjectSpawner.cs
@@ -0,0 +1,24 @@
+using CycloneGames.Core;
+
+namespace CycloneGames.GameplayFramework.Sample.PureUnity
+{
+    public class UnitySampleObjectSpawner : IObjectSpawner
+    {
+        public UnityEngine.Object SpawnObject(UnityEngine.Object original)
+        {
+            return UnityEngine.Object.Instantiate(original);
+        }
+
+        public UnityEngine.Object SpawnObject<T>(T original) where T : UnityEngine.Object
+        {
+            return UnityEngine.Object.Instantiate(original);
+        }
+
+        UnityEngine.GameObject IObjectSpawner.SpawnObjectOnNewGameObject<TComponent>(string name)
+        {
+            var go = new UnityEngine.GameObject(name);
+            var component = go.AddComponent<TComponent>();
+            return go;
+        }
+    }
+}
