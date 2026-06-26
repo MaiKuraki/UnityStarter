66727961459330e136bef90e284ee111d9a1fa6b
Mai Kuraki <mai.k@live.com>
2026-06-27 02:03:19 +0800

    fix warning for Unity6000

--- UnityStarter/Assets/ThirdParty/CycloneGames/CycloneGames.GameplayTags/Editor/GameplayTagEditorWindow.cs
+++ UnityStarter/Assets/ThirdParty/CycloneGames/CycloneGames.GameplayTags/Editor/GameplayTagEditorWindow.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEditor.IMGUI.Controls;
 using UnityEngine;
+
+#if UNITY_6000_0_OR_NEWER
+using TreeViewState = UnityEditor.IMGUI.Controls.TreeViewState<int>;
+#else
+using TreeViewState = UnityEditor.IMGUI.Controls.TreeViewState;
+#endif
+
 using CycloneGames.GameplayTags.Core;
 
 namespace CycloneGames.GameplayTags.Unity.Editor
